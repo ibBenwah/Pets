59884140bf2b238e3951c80c319c1c6d97c07c55
Eddy <eduardo.di.garcia@gmail.com>
2022-08-08 21:50:11 -0400

    Added Pet Class

---
+++ Pets/Pet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Pets
+{
+    internal abstract class Pet
+    {
+        public string Name { get; set; }
+        public virtual void MakeHappyNoise()
+        {
+            Console.WriteLine("caw caw!");
+        }
+    }
+}
+
